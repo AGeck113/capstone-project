2a9f924af391199a9fbfc7018096fad34e4032bc
Andreas Geckler <ageckler@web.de>
2023-01-20 13:32:30 +0100

    add links to the homepage for testing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
 export default function HomePage() {
-  return <h1>Hello from Next.js</h1>;
+  return (
+    <>
+      <h1>Car App</h1>
+      <Link href="/profile/WAUZZZ8V9LA015917">Test Detail Page Car 1</Link>
+      <Link href="/profile/W0L0SDL08D0294820">Test Detail Page Car 2</Link>
+    </>
+  );
 }
