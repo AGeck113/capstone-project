bcbaab60b012e8b2211dd85c6886a82198356ac8
Andreas Geckler <ageckler@web.de>
2023-01-25 12:09:25 +0100

    remove Image

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,17 @@ export default function HomePage() {
     <>
       <h1>Car App</h1>
 
-      <Link href={`/profile/WAUZZZ8V9LA015917`}>
+      {/* <Link href={`/profile/WAUZZZ8V9LA015917`}>
         <Image
           alt="usercar"
           src="https://images.unsplash.com/photo-1579631962852-306c90e1c91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
           width={200}
           height={200}
         />
-      </Link>
+      </Link> */}
 
-      <Link href="/profile/W0L0SDL08D0294820">Test Detail Page Car 2</Link>
+      <Link href="/profile/W0L0SDL08D0294820">Test Detail Page </Link>
+      <Link href="/profile">Create car</Link>
     </>
   );
 }
