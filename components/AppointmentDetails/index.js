8197afcefe1448907687624e015856b19ee78b2c
Andreas Geckler <ageckler@web.de>
2023-02-08 09:05:34 +0100

    delete space

--- components/AppointmentDetails/index.js
+++ components/AppointmentDetails/index.js
@@ -105,7 +105,7 @@ export default function Details({
                           onDelete(document._id);
                         }}
                       >
-                        <span aria-label="Delete"> ❌</span>
+                        <span aria-label="Delete">❌</span>
                       </button>
                     </li>
                   );
