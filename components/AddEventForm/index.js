649a18f4e2f1e05953c22523651d1d0fd69a9f66
Andreas Geckler <ageckler@web.de>
2023-02-08 16:18:02 +0100

    fix required

--- components/AddEventForm/index.js
+++ components/AddEventForm/index.js
@@ -101,7 +101,7 @@ export default function AddEventForm({ onSubmit, appointment }) {
           type="date"
           name="date"
           defaultValue={appointment ? appointment.date : null}
-          required={type === "wishlist" ? true : false}
+          required
           min="2020-01-01"
           max="2030-12-31"
         ></input>
