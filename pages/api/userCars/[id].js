258a2201e3988e4258f3decf05ec80200e83fab5
Andreas Geckler <ageckler@web.de>
2023-02-10 14:47:17 +0100

    update userCars [id].js

--- pages/api/userCars/[id].js
+++ pages/api/userCars/[id].js
@@ -1,28 +1,33 @@
 import dbConnect from "../../../db/connect";
 import UserCar from "../../../db/models/UserCar";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
   await dbConnect();
+  const token = await getToken({ req: request });
   const { id } = request.query;
-  if (request.method === "GET") {
-    const carData = await UserCar.findOne({ UserId: id });
+  if (token) {
+    if (request.method === "GET") {
+      const carData = await UserCar.findOne({ UserId: id });
 
-    if (!carData) {
-      return response.status(404).json({ status: "Not Found" });
-    }
-
-    return response.status(200).json(carData);
-  }
-  if (request.method === "PUT") {
-    const updatedCar = await UserCar.findOneAndUpdate(
-      { UserId: id },
-      {
-        $set: request.body,
+      if (!carData) {
+        return response.status(404).json({ status: "Not Found" });
       }
-    );
-    if (!updatedCar) {
-      return response.status(404).json({ status: "Not Found" });
+
+      return response.status(200).json(carData);
     }
-    return response.status(200).json(updatedCar);
+    if (request.method === "PUT") {
+      const updatedCar = await UserCar.findOneAndUpdate(
+        { UserId: id },
+        {
+          $set: request.body,
+        }
+      );
+      if (!updatedCar) {
+        return response.status(404).json({ status: "Not Found" });
+      }
+      return response.status(200).json(updatedCar);
+    }
   }
+  return response.status(403).json({ status: "Not logged in!" });
 }
