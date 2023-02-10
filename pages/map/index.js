591a0df493cd790183012175b5f1720787209397
Andreas Geckler <ageckler@web.de>
2023-02-10 14:46:09 +0100

    install next-auth

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import Link from "next/link";
 
 export default function EventMap() {
   const Map = dynamic(() => import("../../components/Map"), { ssr: false });
